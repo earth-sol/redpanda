1ae2796d316b9a8d310b1f7dab92d2d16b925ea5
Bharath Vissapragada <bharathv@redpanda.com>
2025-02-04 16:24:03 -0800

    raft/tests: deflake truncation_detection_test
    The current logic to wait on `enqueued` stage seems incorrect and is
    causing the test to flake. `eneuqued` only guarantees that the request
    is enqueued in the raft layer and subsequent replication may still fail
    with not_leader error. So the sequence of flakiness is something like
    this.
    
    1. wait for enqueued futures to resolve
    2. force a step down
    3. wait for replicated entry to be truncated.
    
    If (2) happens before the entry is appended to the leader log, the
    replication future fails with a not leader resulting in a test retry.
    Effectively (1) is not a tight enough condition to wait on to force
    a step down.
    
    This commit rewrites the test to replicate with a leader_ack and wait
    for the future to be resolved (which guarantees a lleader append) and
    then the replication monitor waiter should be resolved with a truncation
    error after leadership change.
    
    Additionally the test seems to be missing some cleanup between
    retries, which is fixed
    
    Signed-off-by: Michał Maślanka <michal@redpanda.com>

--- src/v/raft/tests/raft_fixture.h
+++ src/v/raft/tests/raft_fixture.h
@@ -145,6 +145,8 @@ public:
 
     void on_dispatch(dispatch_callback_t f);
 
+    void reset_dispatch_handlers();
+
     ss::future<> stop();
 
 private:
@@ -260,6 +262,7 @@ public:
     //// \param f The callback function to be invoked when a message is
     /// dispatched.
     void on_dispatch(dispatch_callback_t);
+    void reset_dispatch_handlers();
 
     ss::shared_ptr<in_memory_test_protocol> get_protocol() { return _protocol; }
 
