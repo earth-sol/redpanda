73d109d6c255b7536bd7273fdda820cfc43fae77
Ioannis Kavvadias <ioannis.kavvadias@redpanda.com>
2025-02-13 00:42:18 +0000

    pandaproxy/sr: protobuf rendering: add test for custom option with repeated fields
    (cherry picked from commit 4a6d3bed1849cfef58f29962f402fbef684293ef)

--- src/v/pandaproxy/schema_registry/test/protobuf_rendering.cc
+++ src/v/pandaproxy/schema_registry/test/protobuf_rendering.cc
@@ -164,4 +164,5 @@ INSTANTIATE_TEST_SUITE_P(
     "extension_ranges",
     "composite_extension",
     "nested_extension",
-    "map_entry_hack"));
+    "map_entry_hack",
+    "repeated_custom_options"));
