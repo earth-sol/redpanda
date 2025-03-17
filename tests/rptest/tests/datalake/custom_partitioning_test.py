e5f8f50fb7bc5f9fe8c92c1b60154e59103630d5
Oren Leiman <oren.leiman@redpanda.com>
2025-03-17 11:26:21 -0700

    dl/dt: Pathological partitioning test
    Basically maps each record to its own partition. Nodes OOM in the multiplexor
    
    (cherry picked from commit 92126bb71130488bde4ccd64786532b41515ed80)

--- tests/rptest/tests/datalake/custom_partitioning_test.py
+++ tests/rptest/tests/datalake/custom_partitioning_test.py
@@ -9,6 +9,7 @@
 import itertools
 import time
 import random
+import re
 from uuid import uuid4
 
 from requests.exceptions import HTTPError
@@ -18,6 +19,7 @@ from confluent_kafka.schema_registry.avro import AvroSerializer
 from confluent_kafka.serialization import StringSerializer
 from ducktape.mark import matrix
 
+from rptest.services.catalog_service import CatalogType
 from rptest.clients.rpk import RpkTool, RpkException
 from rptest.services.cluster import cluster
 from rptest.services.redpanda import SISettings, SchemaRegistryConfig
@@ -29,6 +31,11 @@ from rptest.tests.datalake.utils import supported_storage_types
 from rptest.tests.redpanda_test import RedpandaTest
 from rptest.tests.datalake.catalog_service_factory import supported_catalog_types
 from rptest.utils.rpcn_utils import counter_stream_config
+
+OOM_ALLOW_LIST = [
+    # partitioning_writer.cc:65 - Failed to create new writer: Memory exhausted
+    re.compile("Failed to create new writer: Memory exhausted")
+]
 
 
 class DatalakeCustomPartitioningConfigTest(RedpandaTest):
@@ -140,17 +147,25 @@ class DatalakeCustomPartitioningTest(RedpandaTest):
             'value.serializer': value_serializer,
         })
 
-    def produce(self, dl, producer, topic_name, msg_count, already_produced=0):
+    def produce(self,
+                dl,
+                producer,
+                topic_name,
+                msg_count,
+                already_produced=0,
+                gen_record=None,
+                wait_time_s=30):
         # Have all records share the same timestamp, so that they are
         # guaranteed to end up in the same hour partition.
         timestamp = time.time()
         for i in range(msg_count):
             ev_type = random.choice(["type_A", "type_B"])
-            record = {
-                "event_type": ev_type,
-                "number": already_produced + i,
-                "timestamp_us": int(timestamp * 1000000),
-            }
+            record = gen_record(already_produced + i, ev_type, timestamp) if gen_record is not None \
+                else {
+                        "event_type": ev_type,
+                        "number": already_produced + i,
+                        "timestamp_us": int(timestamp * 1000000),
+                }
             producer.produce(
                 topic=topic_name,
                 # key to ensure that all partitions get some records
@@ -158,7 +173,8 @@ class DatalakeCustomPartitioningTest(RedpandaTest):
                 value=record)
         producer.flush()
         dl.wait_for_translation(topic_name,
-                                msg_count=already_produced + msg_count)
+                                msg_count=already_produced + msg_count,
+                                timeout=wait_time_s)
 
     def describe_partitioning(self, dl, topic_name):
         table_name = f"redpanda.{topic_name}"
@@ -171,6 +187,43 @@ class DatalakeCustomPartitioningTest(RedpandaTest):
         return list(
             itertools.dropwhile(lambda r: not r[0].startswith('# Partition'),
                                 spark_describe_out))
+
+    def partition_paths_from_files(self, spark, table_name):
+        partition_path_pattern = re.compile(
+            r".*/data/(?P<partition_path>.*)/.*parquet")
+        empty_partition_path_pattern = re.compile(r".*/data/.*parquet")
+        all_files = spark.run_query_fetch_all(
+            f"select file_path from {table_name}.files")
+        all_partitions = defaultdict(int)
+        for f in all_files:
+            m = partition_path_pattern.match(f[0])
+            if m:
+                partition_path = m.group("partition_path")
+                # remove the partition path from the file name
+                all_partitions[partition_path] += 1
+            else:
+                empty_match = empty_partition_path_pattern.match(f[0])
+                assert empty_match, f"unexpected file path: {f[0]}"
+                all_partitions[""] += 1
+        self.logger.debug("%s partition paths: %s", table_name, all_partitions)
+        return all_partitions
+
+    def assert_at_least_n_files_per_iceberg_partition(
+            self, files_per_partition: dict, n: int):
+        for partition, file_cnt in files_per_partition.items():
+            assert file_cnt >= n, f"partition {partition} has {file_cnt} files, expected at least {n}"
+
+    def list_files(self,
+                   dl: DatalakeServices,
+                   query_engine: QueryEngineType,
+                   topic_name: str,
+                   select="file_path"):
+        if query_engine == QueryEngineType.SPARK:
+            return set(dl.spark().run_query_fetch_all(
+                f"select {select} from redpanda.{topic_name}.files"))
+        elif query_engine == QueryEngineType.TRINO:
+            return set(dl.trino().run_query_fetch_all(
+                f'select {select} from redpanda."{topic_name}$files"'))
 
     @cluster(num_nodes=6)
     @matrix(cloud_storage_type=supported_storage_types(),
@@ -506,3 +559,50 @@ class DatalakeCustomPartitioningTest(RedpandaTest):
 
             connect.stop_stream("ducky_stream", should_finish=False)
             verifier.wait()
+
+    @cluster(num_nodes=6, log_allow_list=OOM_ALLOW_LIST)
+    @matrix(cloud_storage_type=supported_storage_types(),
+            catalog_type=[CatalogType.REST_JDBC])
+    def test_many_partitions(self, cloud_storage_type, catalog_type):
+        with DatalakeServices(self.test_context,
+                              redpanda=self.redpanda,
+                              catalog_type=catalog_type,
+                              include_query_engines=[QueryEngineType.SPARK
+                                                     ]) as dl:
+            topic_name = "foo"
+            msg_count = 50000
+            partitions = 5
+            dl.create_iceberg_enabled_topic(
+                topic_name,
+                partitions=partitions,
+                iceberg_mode="value_schema_id_prefix",
+                config={
+                    "redpanda.iceberg.partition.spec": "(timestamp_us)",
+                })
+
+            producer = self.create_producer(AVRO_SCHEMA_STR)
+            self.produce(dl,
+                         producer,
+                         topic_name,
+                         msg_count,
+                         0,
+                         gen_record=lambda n, ev, t: {
+                             "event_type": ev,
+                             "number": n,
+                             "timestamp_us": int(t + n),
+                         },
+                         wait_time_s=300)
+
+            describe_partitioning = self.describe_partitioning(dl, topic_name)
+            expected_partitioning = [
+                ('# Partition Information', '', ''),
+                ('# col_name', 'data_type', 'comment'),
+                ('timestamp_us', 'timestamp_ntz', None),
+            ]
+
+            assert describe_partitioning == expected_partitioning, \
+                    f"{expected_partitioning=}, got {describe_partitioning=}"
+
+            files = self.list_files(dl, QueryEngineType.SPARK, topic_name)
+            assert len(files) ==  msg_count, \
+                f"Expected {partitions * msg_count} files, got {len(files)}"
