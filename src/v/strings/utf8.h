564cc81304ab9077ba6e9203cf461f8b30edacf6
Michael Boquard <michael@redpanda.com>
2025-03-20 13:30:03 -0400

    utf8: Create invalid_character_exception
    This new exception will be thrown by validate_utf8 and
    validate_control_characters in order to differentiate these errors from
    other runtime errors.
    
    Signed-off-by: Michael Boquard <michael@redpanda.com>
    (cherry picked from commit 72b47308a791a36049c0a4b668776030ee60dc6a)

--- src/v/strings/utf8.h
+++ src/v/strings/utf8.h
@@ -18,7 +18,6 @@
 
 #include <string>
 #include <string_view>
-#include <vector>
 
 /*
  * Used to access/modify the flag that permits/prevents reporting an error
@@ -63,13 +62,29 @@ inline std::string replace_control_chars_in_string(std::string_view s) {
     return rv;
 }
 
+/// Used to report an invalid character
+struct invalid_character_exception : public std::runtime_error {
+    using std::runtime_error::runtime_error;
+};
+
+/// Used to report an invalid UTF8 character
+struct invalid_utf8_exception : public invalid_character_exception {
+    using invalid_character_exception::invalid_character_exception;
+};
+
 template<typename T>
 concept ExceptionThrower = requires(T obj) { obj.conversion_error(); };
 
 struct default_utf8_thrower {
     [[noreturn]] [[gnu::cold]] void conversion_error() {
-        throw std::runtime_error("Cannot decode string as UTF8");
+        throw invalid_utf8_exception("Cannot decode string as UTF8");
     }
+};
+
+/// Used to report a control character being present
+struct control_character_present_exception
+  : public invalid_character_exception {
+    using invalid_character_exception::invalid_character_exception;
 };
 
 struct default_control_character_thrower {
@@ -78,7 +93,7 @@ struct default_control_character_thrower {
       : sanitized_string(replace_control_chars_in_string(unsanitized_string)) {}
     virtual ~default_control_character_thrower() = default;
     [[noreturn]] [[gnu::cold]] virtual void conversion_error() {
-        throw std::runtime_error(
+        throw control_character_present_exception(
           "String contains control character: " + sanitized_string);
     }
 
