b2316545fdb438174406ef5401de1f4e25f0075e
Ioannis Kavvadias <ioannis.kavvadias@redpanda.com>
2025-04-04 15:27:37 +0100

    pandaproxy/sr: Allow soft-deleted references for avro
    (cherry picked from commit 06dbbc6eef7390e03d30f9c19db25c940c8f0d5e)

--- tests/rptest/tests/schema_registry_test.py
+++ tests/rptest/tests/schema_registry_test.py
@@ -613,12 +613,36 @@ message ProtoType {
   float f =  1;
 }"""
 
+schema_avro_def = """
+{
+    "type": "record",
+    "name": "myrecord",
+    "fields": [
+        {
+            "name": "f1",
+            "type": "string"
+        }
+    ]
+}"""
+
 schema_proto_dependee_def = """
 syntax = "proto3";
 
 import "schema_proto.proto";
 message Dependee {
   ProtoType p =  1;
+}"""
+
+schema_avro_dependee_def = """
+{
+    "type": "record",
+    "name": "myotherrecord",
+    "fields": [
+        {
+            "name": "f2",
+            "type": "string"
+        }
+    ]
 }"""
 
 soft_deleted_schemas = {
@@ -631,6 +655,11 @@ soft_deleted_schemas = {
         "subject": "schema_json",
         "schema": json_number_schema_def,
         "type": "JSON",
+    },
+    "avro": {
+        "subject": "schema_avro",
+        "schema": schema_avro_def,
+        "type": "AVRO",
     }
 }
 dependent_schemas = {
@@ -647,7 +676,7 @@ dependent_schemas = {
         "type":
         "PROTOBUF",
         "id":
-        3,
+        4,
     },
     "json": {
         "subject":
@@ -662,7 +691,22 @@ dependent_schemas = {
         "type":
         "JSON",
         "id":
-        4,
+        5,
+    },
+    "avro": {
+        "subject":
+        "schema_avro_dependee_schema",
+        "schema":
+        schema_avro_dependee_def,
+        "references": [{
+            "name": "schema_avro.avro",
+            "subject": "schema_avro",
+            "version": 1
+        }],
+        "type":
+        "AVRO",
+        "id":
+        6,
     },
 }
 
@@ -3391,7 +3435,7 @@ class SchemaRegistryTestMethods(SchemaRegistryEndpoints):
 
         #Test setup. Insert and soft-delete schemas to be referencedby
 
-        for name in ["proto", "json"]:
+        for name in ["proto", "json", "avro"]:
             schema = soft_deleted_schemas[name]
             result_raw = self._post_subjects_subject_versions(
                 subject=schema["subject"],
@@ -3401,16 +3445,16 @@ class SchemaRegistryTestMethods(SchemaRegistryEndpoints):
                 }))
             assert result_raw.status_code == requests.codes.ok, \
                     f"Expected {requests.codes.ok} but got {result_raw.status_code} during test setup. "\
-                    f"Request content: {result_raw.content}"
+                    f"Request content: {result_raw.content}. Processing {name}."
 
             result_raw = self._delete_subject_version(
                 subject=schema["subject"], version=1)
             assert result_raw.status_code == requests.codes.ok, \
                     f"Expected {requests.codes.ok} but got {result_raw.status_code} during test setup. "\
-                    f"Request content: {result_raw.content}"
+                    f"Request content: {result_raw.content}. Processing {name}."
 
         #Register schemas that reference the soft-deleted schemas
-        for name in ["proto", "json"]:
+        for name in ["proto", "json", "avro"]:
             schema = dependent_schemas[name]
             result_raw = self._post_subjects_subject_versions(
                 subject=schema["subject"],
@@ -3421,11 +3465,11 @@ class SchemaRegistryTestMethods(SchemaRegistryEndpoints):
                 }))
             assert result_raw.status_code == requests.codes.ok, \
                     f"Expected {requests.codes.ok} but got {result_raw.status_code}. "\
-                    f"Request content: {result_raw.content}"
+                    f"Request content: {result_raw.content}. Processing {name}."
             result_id = result_raw.json()["id"]
             assert result_id == schema["id"], \
                     f"Expected id {schema['id']} but got {result_id}. "\
-                    f"Request content: {result_raw.content}"
+                    f"Request content: {result_raw.content}. Processing {name}."
 
 
 class SchemaRegistryModeNotMutableTest(SchemaRegistryEndpoints):
