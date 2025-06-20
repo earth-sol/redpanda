earth-sol/redpanda

commit 8cfa04a8a11af6968622842963c53b75ff8588e4

`raft`: don't rethrow in `state_machine_manager::background_apply_fiber()`
Exceptions rethrown here are not caught at any level above
`background_apply_fiber()`, and can lead to a full shutdown of the
backgrounded `apply()` fiber that was launched in `state_machine_manager::start()`.

This is asymmetrical to `try_apply_in_foreground()`, where all exceptions are
caught and are not rethrown. The hope is that errors are transient, and
future attempts in the detached `apply()` fiber will succeed.

This has recently manifested as log lines in `random_node_operations_test`,
in which a failure to download a manifest for the `archival_metadata_stm`
lead to an exception being thrown and not caught, with a log line like:

```
WARN  2025-06-16 06:03:33,830 [shard 0:main] seastar - Exceptional future ignored: std::runtime_error (couldn't download manifest: cloud_storage::error_outcome:2), backtrace: 0x83d351b 0x8125ee3 0x209eb53 0x76afa3f 0x209ec17 0x81d7127 0x81d4753
```

This is a transient error, and instead of breaking the entire `apply()` fiber,
should just log an error and be retried on the next iteration.

It also looks like the historical context for why this `rethrow` statement
exists is because the code in `background_apply_fiber()` seems mostly
plucked from `apply_raft_snapshot()`, where the exception would have been
caught at a higher level.

Also, add a `ss::sleep_abortable()` call to prevent a busy wait.

src/v/raft/state_machine_manager.cc
@@ -626,8 +626,8 @@ ss::future<> state_machine_manager::background_apply_fiber(
                 // do not log known shutdown exceptions as errors
                 if (!ssx::is_shutdown_exception(e)) {
                     vlog(_log.error, "error applying raft snapshot - {}", e);
+                    co_await ss::sleep_abortable(100ms, _as);
                 }
-                std::rethrow_exception(e);
             }
             continue;
         }

commit dae1533f401b9771f51d1395abf99a4b27947c5c

`raft`: rethrow shutdown exception from `background_apply_fiber()`
Commit 8cfa04a8a11af6968622842963c53b75ff8588e4 introduced a potential regression
in that shutdown exceptions would no longer be thrown to early terminate the
backgrounded apply loop.

Add back a rethrow in the case of a shutdown exception.

src/v/raft/state_machine_manager.cc
@@ -653,7 +653,9 @@ ss::future<> state_machine_manager::background_apply_fiber(
             if (fut.failed()) {
                 const auto e = fut.get_exception();
                 // do not log known shutdown exceptions as errors
-                if (!ssx::is_shutdown_exception(e)) {
+                if (ssx::is_shutdown_exception(e)) {
+                    std::rethrow_exception(e);
+                } else {
                     vlog(_log.error, "error applying raft snapshot - {}", e);
                     co_await ss::sleep_abortable(100ms, _as);
                 }