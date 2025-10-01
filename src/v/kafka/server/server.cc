ba4a60857f4cc0028db013bcda9b8689a2576aea
Ben Pope <ben@redpanda.com>
2025-10-01 11:35:52 +0100

    kafka/server: Refactor offset_fetch
    No functional change.
    
    * Extract functionality into functions.
    
    Signed-off-by: Ben Pope <ben@redpanda.com>

--- src/v/kafka/server/server.cc
+++ src/v/kafka/server/server.cc
@@ -24,6 +24,7 @@
 #include "features/enterprise_feature_messages.h"
 #include "features/feature_table.h"
 #include "kafka/protocol/errors.h"
+#include "kafka/protocol/offset_fetch.h"
 #include "kafka/protocol/produce.h"
 #include "kafka/protocol/schemata/list_groups_response.h"
 #include "kafka/server/connection_context.h"
@@ -1085,91 +1086,122 @@ ss::future<response_ptr> add_partitions_to_txn_handler::handle(
     });
 }
 
+namespace {
+
+auto partition_auth_describe(
+  request_context& ctx,
+  auto& resources,
+  auto projection,
+  authz_quiet quiet_authz) {
+    return std::ranges::partition(
+      resources,
+      [&ctx, quiet_authz](const auto& resource) {
+          return ctx.authorized(
+            security::acl_operation::describe, resource, quiet_authz);
+      },
+      projection);
+};
+
+} // namespace
+
 template<>
 ss::future<response_ptr>
 offset_fetch_handler::handle(request_context ctx, ss::smp_service_group) {
     offset_fetch_request request;
     request.decode(ctx.reader(), ctx.header().version);
     log_request(ctx.header(), request);
+
     if (!ctx.authorized(
           security::acl_operation::describe, request.data.group_id)) {
-        if (!ctx.audit()) {
-            co_return co_await ctx.respond(
-              offset_fetch_response(error_code::broker_not_available));
-        } else {
-            co_return co_await ctx.respond(
-              offset_fetch_response(error_code::group_authorization_failed));
-        }
+        auto error = !ctx.audit() ? error_code::broker_not_available
+                                  : error_code::group_authorization_failed;
+        co_return co_await ctx.respond(offset_fetch_response(error));
     }
 
-    /*
-     * request is for all group offsets
-     */
-    if (!request.data.topics) {
-        auto resp = co_await ctx.groups().offset_fetch(std::move(request));
-        if (resp.data.error_code != error_code::none) {
-            co_return co_await ctx.respond(std::move(resp));
-        }
+    constexpr auto has_topics = [](const auto& g) {
+        return g.topics.has_value();
+    };
 
-        // remove unauthorized topics from response
-        auto unauthorized = std::partition(
-          resp.data.topics.begin(),
-          resp.data.topics.end(),
-          [&ctx](const offset_fetch_response_topic& topic) {
-              /*
-               * quiet authz failures. this is checking for visibility across
-               * all topics not specifically requested topics.
-               */
-              return ctx.authorized(
-                security::acl_operation::describe,
-                topic.name,
-                authz_quiet{true});
-          });
+    constexpr auto post_filter_authorized_topics = [](auto& ctx, auto& group) {
+        if (group.error_code != error_code::none) {
+            return;
+        }
+        /*
+         * quiet authz failures. this is checking for visibility across
+         * all topics not specifically requested topics.
+         */
+        auto unauthorized_rng = partition_auth_describe(
+          ctx,
+          group.topics,
+          &offset_fetch_response_topic::name,
+          authz_quiet::yes);
 
         if (!ctx.audit()) {
-            resp.data.topics.clear();
-            resp.data.error_code = error_code::broker_not_available;
-            co_return co_await ctx.respond(std::move(resp));
+            group.topics.clear();
+            group.error_code = error_code::broker_not_available;
+            return;
         }
 
-        resp.data.topics.erase_to_end(unauthorized);
+        group.topics.erase_to_end(unauthorized_rng.begin());
+        return;
+    };
 
-        co_return co_await ctx.respond(std::move(resp));
-    }
+    constexpr auto pre_filter_authorized_topics = [](auto& ctx, auto& group) {
+        offset_fetch_response_data response;
+
+        auto unauthorized_rng = partition_auth_describe(
+          ctx,
+          group.topics.value(),
+          &offset_fetch_request_topic::name,
+          authz_quiet::no);
+
+        if (!ctx.audit()) {
+            response.error_code = error_code::broker_not_available;
+            return response;
+        }
+
+        chunked_vector<offset_fetch_request_topic> unauthorized{
+          std::from_range, unauthorized_rng | std::views::as_rvalue};
+
+        // remove unauthorized topics from request
+        group.topics->erase_to_end(unauthorized_rng.begin());
+
+        // add requested (but unauthorized) topics into response
+        for (auto& req_topic : unauthorized) {
+            auto& topic = response.topics.emplace_back();
+            topic.name = std::move(req_topic.name);
+            for (auto partition_index : req_topic.partition_indexes) {
+                auto& partition = topic.partitions.emplace_back();
+                partition.partition_index = partition_index;
+                partition.committed_offset = model::offset{-1};
+                partition.error_code = error_code::topic_authorization_failed;
+            }
+        }
+        return response;
+    };
+
+    std::optional<offset_fetch_response_data> unauthorized;
 
     /*
      * pre-filter authorized topics in request
      */
-    auto unauthorized_it = std::partition(
-      request.data.topics->begin(),
-      request.data.topics->end(),
-      [&ctx](const offset_fetch_request_topic& topic) {
-          return ctx.authorized(security::acl_operation::describe, topic.name);
-      });
-
-    if (!ctx.audit()) {
-        co_return co_await ctx.respond(
-          offset_fetch_response(error_code::broker_not_available));
+    if (has_topics(request.data)) {
+        unauthorized.emplace(pre_filter_authorized_topics(ctx, request.data));
     }
 
-    chunked_vector<offset_fetch_request_topic> unauthorized(
-      std::make_move_iterator(unauthorized_it),
-      std::make_move_iterator(request.data.topics->end()));
-
-    // remove unauthorized topics from request
-    request.data.topics->erase_to_end(unauthorized_it);
     auto resp = co_await ctx.groups().offset_fetch(std::move(request));
 
-    // add requested (but unauthorized) topics into response
-    for (auto& req_topic : unauthorized) {
-        auto& topic = resp.data.topics.emplace_back();
-        topic.name = std::move(req_topic.name);
-        for (auto partition_index : req_topic.partition_indexes) {
-            auto& partition = topic.partitions.emplace_back();
-            partition.partition_index = partition_index;
-            partition.committed_offset = model::offset{-1};
-            partition.error_code = error_code::topic_authorization_failed;
-        }
+    if (unauthorized.has_value()) {
+        /*
+         * merge pre-filtered unauthorized topics into response
+         */
+        std::ranges::move(
+          unauthorized->topics, std::back_inserter(resp.data.topics));
+    } else {
+        /*
+         * post-filter unauthorized topics in response
+         */
+        post_filter_authorized_topics(ctx, resp.data);
     }
 
     co_return co_await ctx.respond(std::move(resp));
