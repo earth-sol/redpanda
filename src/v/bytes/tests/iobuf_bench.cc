19859d293991d0170d6325ec3b1a69d699912f29
Tyler Rockwood <rockwood@redpanda.com>
2025-10-14 15:12:17 +0000

    iobuf: optimize <=>
    Before:
    
    ```
    test                       iterations      median         mad         min         max      allocs       tasks        inst      cycles
    iobuf.cmp_bench_small        25260000    39.563ns     0.129ns    39.251ns    39.787ns       0.000       0.000       594.3       161.0
    iobuf.cmp_bench_medium          10000   135.743us    17.305ns   135.711us   135.779us       0.000       0.000   2396246.6    553506.3
    iobuf.cmp_bench_large            5000   436.648us    57.891ns   436.434us   504.353us       0.000       0.000   7707794.9   1836157.7
    ```
    
    After:
    
    ```
    test                       iterations      median         mad         min         max      allocs       tasks        inst      cycles
    iobuf.cmp_bench_small       149765000     6.436ns     0.001ns     6.423ns     6.438ns       0.000       0.000        83.5        25.7
    iobuf.cmp_bench_medium         305000     2.844us     0.419ns     2.843us     2.845us       0.000       0.000     23203.5     11544.5
    iobuf.cmp_bench_large           95000     9.596us     1.370ns     9.594us     9.603us       0.000       0.000     77184.9     38924.3
    ```

--- src/v/bytes/tests/iobuf_bench.cc
+++ src/v/bytes/tests/iobuf_bench.cc
@@ -49,8 +49,8 @@ size_t move_bench() {
     return inner_iters * 2;
 }
 
-template<size_t Size>
-size_t eq_bench() {
+template<size_t Size, typename cmp_fn>
+size_t cmp_bench() {
     iobuf a = make_iobuf(Size);
     iobuf a_shared = a.share();
     iobuf a_copy = a.copy();
@@ -63,11 +63,11 @@ size_t eq_bench() {
     }
     perf_tests::start_measuring_time();
     for (auto i = inner_iters; i--;) {
-        perf_tests::do_not_optimize(a == a_shared);
-        perf_tests::do_not_optimize(a == a_copy);
-        perf_tests::do_not_optimize(a == b);
-        perf_tests::do_not_optimize(a_copy == b_copy);
-        perf_tests::do_not_optimize(b_copy == b);
+        perf_tests::do_not_optimize(cmp_fn{}(a, a_shared));
+        perf_tests::do_not_optimize(cmp_fn{}(a, a_copy));
+        perf_tests::do_not_optimize(cmp_fn{}(a, b));
+        perf_tests::do_not_optimize(cmp_fn{}(a_copy, b_copy));
+        perf_tests::do_not_optimize(cmp_fn{}(b_copy, b));
     }
     perf_tests::stop_measuring_time();
     return inner_iters * 5;
@@ -102,9 +102,17 @@ PERF_TEST(iobuf, move_bench_small) { return move_bench<71>(); }
 PERF_TEST(iobuf, move_bench_medium) { return move_bench<300_KiB>(); }
 PERF_TEST(iobuf, move_bench_large) { return move_bench<965_KiB>(); }
 
-PERF_TEST(iobuf, eq_bench_small) { return eq_bench<71>(); }
-PERF_TEST(iobuf, eq_bench_medium) { return eq_bench<300_KiB>(); }
-PERF_TEST(iobuf, eq_bench_large) { return eq_bench<965_KiB>(); }
+PERF_TEST(iobuf, eq_bench_small) { return cmp_bench<71, std::equal_to<>>(); }
+PERF_TEST(iobuf, eq_bench_medium) {
+    return cmp_bench<300_KiB, std::equal_to<>>();
+}
+PERF_TEST(iobuf, eq_bench_large) {
+    return cmp_bench<965_KiB, std::equal_to<>>();
+}
+
+PERF_TEST(iobuf, cmp_bench_small) { return cmp_bench<71, std::less<>>(); }
+PERF_TEST(iobuf, cmp_bench_medium) { return cmp_bench<300_KiB, std::less<>>(); }
+PERF_TEST(iobuf, cmp_bench_large) { return cmp_bench<965_KiB, std::less<>>(); }
 
 PERF_TEST(iobuf, append_bench_small) { return append_bench<1'000, 4>(); }
 PERF_TEST(iobuf, append_bench_medium) { return append_bench<1'000, 40_KiB>(); }
