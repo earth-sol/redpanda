1acf6490c0aa221773f5c3351718e5f27375f9e5
Michael Boquard <michael@redpanda.com>
2025-11-20 09:17:52 -0500

    dt: Update admin password
    FIPS CDT is failing because the admin password is too short and the
    rdkafka based clients (which use OpenSSL in FIPS mode) are failing to
    construct their final client SCRAM message.  This commit updates the
    superuser password to be at least 14 characters long.
    
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- tests/rptest/services/redpanda.py
+++ tests/rptest/services/redpanda.py
@@ -1246,7 +1246,7 @@ class RpkNodeConfig:
 
 class RedpandaServiceConstants:
     SUPERUSER_CREDENTIALS: SaslCredentials = SaslCredentials(
-        "admin", "admin", "SCRAM-SHA-256"
+        "admin", "admin1234567890", "SCRAM-SHA-256"
     )
 
 
