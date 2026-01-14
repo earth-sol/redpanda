4ff74dc9b08df9d429b765daea80c1a80302b13f
Tyler Rockwood <rockwood@redpanda.com>
2026-01-14 16:49:08 -0500

    ct: Add inactive epoch estimation test

--- src/v/cloud_topics/level_zero/stm/tests/ctp_stm_state_test.cc
+++ src/v/cloud_topics/level_zero/stm/tests/ctp_stm_state_test.cc
@@ -152,4 +152,120 @@ TEST(ctp_stm_state_test, advance_start_offset) {
     EXPECT_EQ(state.start_offset(), kafka::offset{5});
 }
 
+ct::cluster_epoch operator""_epoch(unsigned long long v) {
+    return ct::cluster_epoch(static_cast<int64_t>(v));
+}
+
+kafka::offset operator""_offset(unsigned long long v) {
+    return kafka::offset(static_cast<int64_t>(v));
+}
+
+TEST(ctp_stm_state_test, sliding_window_issue) {
+    ct::ctp_stm_state state;
+
+    kafka::offset hwm = 0_offset;
+
+    auto estimate_inactive_epoch =
+      [&state] -> std::optional<ct::cluster_epoch> {
+        auto estimate = state.estimate_min_epoch().transform(
+          ct::prev_cluster_epoch);
+        auto prev = state.get_previous_epoch().transform(
+          ct::prev_cluster_epoch);
+        return std::min(estimate, prev);
+    };
+
+    auto apply_replicated = [&state, &hwm](ct::cluster_epoch epoch) {
+        state.advance_epoch(epoch, kafka::offset_cast(hwm));
+        hwm++;
+    };
+
+    auto reconcile = [&state](kafka::offset offset) {
+        state.advance_last_reconciled_offset(
+          offset, kafka::offset_cast(offset));
+    };
+
+    // Our start state, we can't GC anything and we have to
+    // get the write lock before we can start our window
+    EXPECT_EQ(estimate_inactive_epoch(), std::nullopt);
+    EXPECT_FALSE(state.epoch_in_window(0_epoch));
+
+    // Write lock grabbed, max epoch can be advanced!
+    state.advance_max_seen_epoch(0_epoch);
+
+    // Now epoch 0 is in the window
+    EXPECT_TRUE(state.epoch_in_window(0_epoch));
+    // Epoch 1 is not in the window
+    EXPECT_FALSE(state.epoch_in_window(1_epoch));
+
+    // Now the batch that was replicated with offset 0
+    apply_replicated(0_epoch);
+
+    // Still not safe to GC
+    EXPECT_EQ(estimate_inactive_epoch(), std::nullopt);
+
+    // Let's now add another batch at epoch 0
+    EXPECT_TRUE(state.epoch_in_window(0_epoch));
+    apply_replicated(0_epoch);
+
+    // Reconciler now runs
+    reconcile(hwm);
+
+    // Still not safe to GC
+    EXPECT_EQ(estimate_inactive_epoch(), std::nullopt);
+
+    EXPECT_FALSE(state.epoch_in_window(5_epoch));
+
+    // Epoch is bumped, our window should now be [0, 5]
+    state.advance_max_seen_epoch(5_epoch);
+
+    // This is our new epoch
+    EXPECT_TRUE(state.epoch_in_window(5_epoch));
+    // Our previous epoch is good still
+    EXPECT_TRUE(state.epoch_in_window(0_epoch));
+    // And so is something in between (unlikely in real life, but just to show)
+    EXPECT_TRUE(state.epoch_in_window(3_epoch));
+
+    // Still not safe to GC, we accept stuff at epoch 0
+    EXPECT_EQ(estimate_inactive_epoch(), ct::cluster_epoch::min());
+
+    apply_replicated(5_epoch);
+
+    // Still not safe to GC
+    EXPECT_EQ(estimate_inactive_epoch(), ct::cluster_epoch::min());
+
+    // We can still replicate an epoch at 0
+    apply_replicated(0_epoch);
+
+    // Still not safe to GC
+    EXPECT_EQ(estimate_inactive_epoch(), ct::cluster_epoch::min());
+
+    // Now we start to replicate to the epoch to 10 (write lock grabbed)
+    state.advance_max_seen_epoch(10_epoch);
+    EXPECT_TRUE(state.epoch_in_window(10_epoch));
+    EXPECT_TRUE(state.epoch_in_window(5_epoch));
+    EXPECT_TRUE(state.epoch_in_window(8_epoch));
+    EXPECT_FALSE(state.epoch_in_window(0_epoch));
+    EXPECT_FALSE(state.epoch_in_window(4_epoch));
+
+    apply_replicated(10_epoch);
+
+    EXPECT_EQ(estimate_inactive_epoch(), ct::cluster_epoch::min());
+
+    // Reconcile
+    reconcile(hwm);
+
+    // NOW we can GC everything below the window
+    EXPECT_EQ(estimate_inactive_epoch(), 4_epoch);
+
+    // Now we bump the window again, but haven't replicated it yet.
+    state.advance_max_seen_epoch(15_epoch);
+    EXPECT_TRUE(state.epoch_in_window(10_epoch));
+    EXPECT_TRUE(state.epoch_in_window(15_epoch));
+    EXPECT_TRUE(state.epoch_in_window(12_epoch));
+    EXPECT_FALSE(state.epoch_in_window(9_epoch));
+
+    // The min epoch CANNOT CHANGE YET! The reconciler has not run.
+    EXPECT_EQ(estimate_inactive_epoch(), 4_epoch);
+}
+
 } // anonymous namespace
